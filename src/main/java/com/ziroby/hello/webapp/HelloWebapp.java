37f8252a4998484449bcc8cd93d9d12f7d1fa36e
Mohammad Naghavi <mohamnag@gmail.com>
2016-02-18 14:47:00 +0100

    fixing gradle's integration task

--- src/main/java/com/ziroby/hello/webapp/HelloWebapp.java
+++ src/main/java/com/ziroby/hello/webapp/HelloWebapp.java
@@ -1,10 +1,17 @@
 package com.ziroby.hello.webapp;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-
 import com.ziroby.hello.service.HelloWorldService;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ * This class represents how a POJO can be used to expose functionality
+ * of your Service Layer into a web app.
+ *
+ * TODO: replace this class with similar ones for your demand
+ */
 @Path("/hello")
 public class HelloWebapp {
 	private static HelloWorldService helloWorldService = new HelloWorldService();
@@ -13,4 +20,11 @@ public class HelloWebapp {
 	public String hello() {
 		return helloWorldService.sayHello();
 	}
+
+	@GET()
+	@Path("/{name}")
+	public String helloUser(@PathParam("name") String name) {
+		return helloWorldService.sayHello(name);
+	}
+
 }
--- src/test/java/com/ziroby/hello/webapp/HelloIntegrationTest.java
+++ src/test/java/com/ziroby/hello/webapp/HelloIntegrationTest.java
@@ -1,12 +1,11 @@
 package com.ziroby.hello.webapp;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class HelloIntegrationTest {
 	private static String HELLO_URL = "http://localhost:8080/hello";
@@ -18,5 +17,23 @@ public class HelloIntegrationTest {
 		String response = webResource.get(String.class);
 		
 		assertThat(response, is("Hello, World!"));
+	}
+
+	@Test
+	public void testHelloUser() throws Exception {
+		Client client = Client.create();
+		WebResource webResource = client.resource(HELLO_URL + "/Max");
+		String response = webResource.get(String.class);
+
+		assertThat(response, is("Hello, Max!"));
+	}
+
+	@Test
+	public void testHelloUserQueryParam() throws Exception {
+		Client client = Client.create();
+		WebResource webResource = client.resource(HELLO_URL + "?name=Min");
+		String response = webResource.get(String.class);
+
+		assertThat(response, is("Hello, Min!"));
 	}
 }
